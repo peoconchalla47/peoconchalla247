0a8019981d2b65af084d143a8b85ad842ce2c24b
peoconchalla47 <claudioignacio.moyagarrido@gmail.com>
2025-10-27 15:29:55 -0300

    Add pg.run() to inicio.py for navigation execution
    Added missing pg.run() to execute navigation.

--- inicio.py
+++ inicio.py
@@ -2,3 +2,4 @@ import streamlit as st
 st.set_page_config(page_title="Balon mano", page_icon="🤾‍♀")
 
 pg = st.navigation8(["balonhand.py", "reglasyseminales.py", "balonmano3.py", "cosa2.py", "descarga,jpg", "oip8.jpg"])
+pg.run()
